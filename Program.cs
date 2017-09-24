ffbe8f7f25c5c32961dcc5e0e1bba75644e75ff8
ingxx <523969705@qq.com>
2017-09-24 23:50:44 +0800

    OK

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace 闲鱼搜索
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("输入要搜索的内容:");
+            string text = Console.ReadLine();
+            Process.Start("https://s.2.taobao.com/list/list.htm?q=" + Uri.EscapeUriString(text) + "&search_type=item&_input_charset=utf8 ");
+        }
+    }
+}
